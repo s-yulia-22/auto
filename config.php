75ebf4f0d76d35e4b9a1be1212819ac2f1ef5cb4
Yulia <suzdaleva22@gmail.com>
2021-11-10 16:11:50 +0200

    New form

---
+++ config.php
@@ -0,0 +1,11 @@
+<?
+  // *** Настройка обязательности полей, в случае если они присутствуют в вашей форме
+ 
+  // Имя
+  const NAMEISREQUIRED = true;
+  const MSGSNAMEERROR = "Поле обязательно для заполнения";
+ 
+  // Телефон
+  const TELISREQUIRED = false;
+  const MSGSTELERROR = "Поле обязательно для заполнения";
+  ?>
